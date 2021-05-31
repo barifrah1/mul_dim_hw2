920a3c86fd4b0eee80a81c2662a22d9d56c811c6
barifrah1 <63462394+barifrah1@users.noreply.github.com>
2021-05-31 16:07:37 +0300

    after rami

--- LatentSemanticIndexing.py
+++ LatentSemanticIndexing.py
@@ -5,6 +5,8 @@ from matplotlib import pyplot as plt
 from pandas.core.frame import DataFrame
 import scipy
 from scipy.sparse.linalg import svds
+from scipy.spatial.distance import cdist
+from scipy.spatial import KDTree
 
 
 class LatentSemanticIndexing():
@@ -32,9 +34,9 @@ class LatentSemanticIndexing():
         return docsVectorsComposed, termsVectorsComposed.transpose()
 
 
-def plotDocsAndTermsGraph(docsVecs, termVecs, docsNames, termsNames):
+def plotDocsAndTermsGraph(docsVecs, termVecs, docsNames, termsNames, query_loc='NA',closest_doc_loc = 'NA', word1_loc = 'NA', word2_loc = 'NA'):
 
-    plt.scatter(docsVecs[:, 0], docsVecs[:, 1], color='red')
+    plt.scatter(docsVecs[:, 0], docsVecs[:, 1], color='blue')
     # docs plotting
     for i in range(len(docsVecs)):
 
@@ -46,7 +48,7 @@ def plotDocsAndTermsGraph(docsVecs, termVecs, docsNames, termsNames):
                      textcoords="offset points",  # how to position the text
                      xytext=(0, 10),  # distance from text to points (x,y)
                      ha='center')
-    plt.scatter(termVecs[:, 0], termVecs[:, 1], color='blue')
+    plt.scatter(termVecs[:, 0], termVecs[:, 1], color='green')
     for j in range(len(termVecs)):
         label = f"{termsNames[j]}"
 
@@ -56,9 +58,48 @@ def plotDocsAndTermsGraph(docsVecs, termVecs, docsNames, termsNames):
                      textcoords="offset points",  # how to position the text
                      xytext=(0, 10),  # distance from text to points (x,y)
                      ha='center')
+    if query_loc is not 'NA':
+        label = 'query'
+        plt.annotate("query",query_loc, textcoords="offset points",  # how to position the text
+                     xytext=(0, 10),  # distance from text to points (x,y)
+                     ha='center')
+        plt.scatter(query_loc[0], query_loc[1], color='red')
+
+        plt.annotate("", closest_doc_loc, textcoords="offset points",  # how to position the text
+                     xytext=(0, 10),  # distance from text to points (x,y)
+                     ha='center')
+        plt.scatter(closest_doc_loc[0], closest_doc_loc[1], s = 250, facecolors='none', color='red',marker='o')
+        plt.scatter(word1_loc[0], word1_loc[1], s=250, facecolors='none', color='red', marker='o')
+        plt.scatter(word2_loc[0], word2_loc[1], s=250, facecolors='none', color='red', marker='o')
 
     plt.xlabel("PC1")
     plt.ylabel("PC2")
     plt.title("Geometrical plot")
 
     plt.show()
+
+def word_doc_query(word1, word2, word_component_df,docsVecs, termVecs, docsNames, termsNames):
+    word1_loc = word_component_df[word1]
+    word2_loc = word_component_df[word2]
+    query_loc = [(word1_loc[0] + word2_loc[0]) / 2, (word1_loc[1] + word2_loc[1]) / 2]
+    """
+        idx = (np.linalg.norm(docsVecs - query_loc)).argmin()
+        d=[]
+        for i in range docsVecs:
+            d=cdist(docsVecs[i], query_loc, 'euclidean')
+    """
+
+    kdtree = KDTree(docsVecs)
+    d, closest_doc = kdtree.query((query_loc))
+    print("closest document location:", docsVecs[closest_doc],"\nclosest document number:", closest_doc)
+    closest_doc_loc = docsVecs[closest_doc]
+
+
+    plotDocsAndTermsGraph(docsVecs, termVecs, docsNames, termsNames,query_loc, closest_doc_loc, word1_loc, word2_loc)
+
+
+
+
+
+
+
--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from Args import Args
 from Dataset import Dataset
-from LatentSemanticIndexing import LatentSemanticIndexing, plotDocsAndTermsGraph
+from LatentSemanticIndexing import LatentSemanticIndexing, plotDocsAndTermsGraph, word_doc_query
 
 
 if __name__ == "__main__":
@@ -14,5 +14,12 @@ if __name__ == "__main__":
     docsVectorsComposed, termsVectorsComposed = lsi.getTermsAndDocsVectors()
     print("docs vectors: ", docsVectorsComposed)
     print("terms vectors: ", termsVectorsComposed)
-    plotDocsAndTermsGraph(docsVectorsComposed,
-                          termsVectorsComposed, dataset.docs, dataset.terms)
+    plotDocsAndTermsGraph(docsVectorsComposed,termsVectorsComposed, dataset.docs, dataset.terms)
+
+    word_component_df = pd.DataFrame(termsVectorsComposed.T, index=["component_1", "component_2"], columns = lsi.df.columns)
+    word1 = input("Enter 1st word:")
+    word2 = input("Enter 2nd word:")
+    #word1 = 'cool'
+    #word2 = 'super'
+
+    word_doc_query(word1, word2, word_component_df,docsVectorsComposed,termsVectorsComposed, dataset.docs, dataset.terms)
